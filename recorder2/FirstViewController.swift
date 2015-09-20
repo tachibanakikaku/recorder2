d27117d1aee1b910c2db65c5107d5ce2cdf13d3f
YOSHIOK.A <ashitaba.yoshioka@leonisand.co>
2015-09-20 15:10:53 +0900

    Initial Commit

---
+++ recorder2/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  recorder2
+//
+//  Created by yoshiokaas on 2015/09/20.
+//  Copyright © 2015年 tachibanakikaku.com. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ recorder2Tests/recorder2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  recorder2Tests.swift
+//  recorder2Tests
+//
+//  Created by yoshiokaas on 2015/09/20.
+//  Copyright © 2015年 tachibanakikaku.com. All rights reserved.
+//
+
+import XCTest
+@testable import recorder2
+
+class recorder2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
